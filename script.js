e5d3651a978116d2dfcdac8c1bfaddb9e8f4dd30
Felix <bpr59@comcast.net>
2020-05-08 22:12:16 -0400

    fixed label issue with randSelector item in all conditions and variable element.

--- script.js
+++ script.js
@@ -18,35 +18,36 @@ var lower = document.querySelector("#lowercase").checked;
 var numbers = document.querySelector("#numbers").checked;
 var symbols = document.querySelector("#symbols").checked;
 
-//Setting randomSelector variable and conditions to cycle through values and generate password
+//Setting randSelector variable and conditions to cycle through values and generate password
 
-var randomSelector = [];
+var randSelector = [];
 var password = [];
 
 if (upper === true) {
         for (var i = codeUpper[0]; i <= codeUpper[1]; i++) {
-          randomSelector.push(i);
+          randSelector.push(i);
         }
 }
 
 if (lower === true) {
   for (var i = codeLower[0]; i <= codeLower[1]; i++) {
-    randomSelector.push(i);
+    randSelector.push(i);
   }
 }
 
 if (numbers === true) {
   for (var i = codeNum[0]; i <= codeNum[1]; i++) {
-    randomSelector.push(i);
+    randSelector.push(i);
   }
 }
 
 if (symbols === true) {
   for (var i = codeSymbol[0]; i <= codeSymbol[1]; i++) {
-    randomSelector.push(i);
+    randSelector.push(i);
   }
 }
-
+ 
+//console.log(randSelector);
 
 
 
